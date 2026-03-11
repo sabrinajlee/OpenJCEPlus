04b1d4d8ed656ab1b01a2d6f9eda286f8b682fa7
Sabrina Lee <137124468+sabrinajlee@users.noreply.github.com>
2026-03-11 16:33:16 -0400

    Update LIBPATH export for AIX in runOpenJCEPlus

--- utils.groovy
+++ utils.groovy
@@ -167,7 +167,7 @@ def runOpenJCEPlus(command, software) {
     dir("openjceplus/OpenJCEPlus") {
         def additional_exports = ""
         if (software == "aix") {
-            //additional_exports = "export LIBPATH=$WORKSPACE/openjceplus/OCK/:$WORKSPACE/openjceplus/OCK/jgsk_sdk;"
+            additional_exports = "export LIBPATH=/usr/lib;"
         }
 
         def additional_envars = ADDITIONAL_ENVARS
